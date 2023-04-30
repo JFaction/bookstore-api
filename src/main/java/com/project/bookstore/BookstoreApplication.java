06e70ad001c85d4ff95aeacaf61d8c99037e762a
JFaction <juarezsouza.contato@hotmail.com>
2023-04-30 12:11:07 -0300

    findById para Categoria

--- src/main/java/com/project/bookstore/BookstoreApplication.java
+++ src/main/java/com/project/bookstore/BookstoreApplication.java
@@ -1,16 +1,7 @@
 package com.project.bookstore;
-
-import java.util.Arrays;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.project.bookstore.domain.Categoria;
-import com.project.bookstore.domain.Livro;
-import com.project.bookstore.repositories.CategoriaRepository;
-import com.project.bookstore.repositories.LivroRepository;
 
 @SpringBootApplication
 public class BookstoreApplication{
