635f7b7af927e738d965ffc08e1d0f1678dda2cc
JFaction <juarezsouza.contato@hotmail.com>
2023-04-30 14:45:34 -0300

    findAll para  Categoria utilizando padrao dto

--- src/main/java/com/project/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/project/bookstore/resources/CategoriaResource.java
@@ -1,5 +1,8 @@
 package com.project.bookstore.resources;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.bookstore.domain.Categoria;
+import com.project.bookstore.dtos.CategoriaDTO;
 import com.project.bookstore.service.CategoriaService;
 
 @RestController
@@ -18,9 +22,17 @@ public class CategoriaResource {
     private CategoriaService service;
 
     @GetMapping(value = "/{id}")
-    public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+    public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
         Categoria obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
     }
-    
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll(){
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDTO = list.stream()
+                .map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDTO);
+    }
+
 }
