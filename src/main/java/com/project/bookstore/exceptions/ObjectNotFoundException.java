4b9e7b59ce95a465499766998bbc40300bce9c1a
JFaction <juarezsouza.contato@hotmail.com>
2023-04-30 12:52:55 -0300

    excecao personalizada para objeto nao encontrado

---
+++ src/main/java/com/project/bookstore/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,15 @@
+package com.project.bookstore.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException{
+    private static final long serialVersionUID = 1;
+
+    public ObjectNotFoundException(String message, Throwable cause){
+        super(message, cause);
+    }
+
+    public ObjectNotFoundException(String message){
+        super(message);
+    }
+    
+
+}
